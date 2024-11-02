1048f650931d336d071af9a3801341e83cd2bd13
AlulCode45 <alulgans70@gmail.com>
2024-11-03 00:47:39 +0700

    add: adding permission:

---
+++ database/seeders/RolePermissions.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissions extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // member permissions
+        Permission::create(['name' => 'edit report']);
+        Permission::create(['name' => 'add report']);
+        Permission::create(['name' => 'delete report']);
+        // admin permissions
+        Permission::create(['name' => 'add category']);
+        Permission::create(['name' => 'edit category']);
+        Permission::create(['name' => 'delete category']);
+        Permission::create(['name' => 'add user']);
+        Permission::create(['name' => 'edit user']);
+        Permission::create(['name' => 'delete user']);
+        Permission::create(['name' => 'change user role']);
+        Permission::create(['name' => 'add news']);
+        Permission::create(['name' => 'edit news']);
+        Permission::create(['name' => 'delete news']);
+        // goverment permissions
+        Permission::create(['name' => 'add response']);
+        Permission::create(['name' => 'edit response']);
+        Permission::create(['name' => 'delete response']);
+
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+
+        $role = Role::create(['name' => 'member']);
+        $role->givePermissionTo(
+            ['edit report', 'add report', 'delete report']
+        );
+
+        $role = Role::create(['name' => 'goverment']);
+        $role->givePermissionTo(['add response', 'edit response', 'delete response']);
+
+        $role = Role::create(['name' => 'admin']);
+        $role->givePermissionTo(Permission::all());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
+});
 
 Route::middleware(\App\Http\Middleware\AdminMiddleware::class)->group(function () {
 
