3c798d98846ee39bcae9093d41f564b6920795e4
AlulCode45 <alulgans70@gmail.com>
2024-11-03 01:18:18 +0700

    update: register policies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,30 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
+use App\Models\News;
+use App\Models\Ratings;
+use App\Models\Reports;
+use App\Models\Response;
+use App\Models\User;
+use App\Policies\CategoryPolicy;
+use App\Policies\NewsPolicy;
+use App\Policies\RatingsPolicy;
+use App\Policies\ReportsPolicy;
+use App\Policies\ResponsePolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+    private $policies = [
+        Category::class => CategoryPolicy::class,
+        News::class => NewsPolicy::class,
+        Ratings::class => RatingsPolicy::class,
+        Response::class => ResponsePolicy::class,
+        Reports::class => ReportsPolicy::class,
+    ];
     /**
      * Register any application services.
      */
@@ -19,6 +39,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        foreach ($this->policies as $model => $policy) {
+            Gate::policy($model, $policy);
+        }
     }
 }
